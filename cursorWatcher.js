2e0c92da7bec1d878d406c47e483bb03c34b711c
Rameş Aliyev <creator@ramesaliyev.com>
2014-02-18 20:42:50 +0200

    Performans ve okunabilirlik iyileştirmeleri.
    Performance and readibility improvements.

--- cursorWatcher.js
+++ cursorWatcher.js
@@ -5,61 +5,95 @@
  * @param options
  * @returns {*}
  */
-jQuery.fn.cursorWatcher = function (options) {
-
-    var option = jQuery.extend({
-        always: function () {
-        },
-        center: function () {
-        },
-        top: function () {
-        },
-        bottom: function () {
-        },
-        left: function () {
-        },
-        right: function () {
-        },
-        topLeft: function () {
-        },
-        topRight: function () {
-        },
-        bottomLeft: function () {
-        },
-        bottomRight: function () {
-        },
-    }, options);
+jQuery.fn.cursorWatcher = function (option) {
 
     return this.each(function () {
 
-        var target = $(this)
-            , width = target.width()
-            , height = target.height();
+        // Get elements & values.
+        var target  = this, // Target should be pure dom element.
+            $target = $(target),
+            width = $target.width(),
+            height = $target.height();
 
+        // Listen document mousemove event.
         $(document).on('mousemove', function (e) {
 
-            var offset = target.offset();
+            // Get offset.
+            var offset = $target.offset();
 
-            option.always(target);
+            // Edges
+            var topEdge    = offset.top,
+                leftEdge   = offset.left,
+                rightEdge  = leftEdge + width,
+                bottomEdge = topEdge + height;
 
-            if (e.pageY < offset.top && e.pageX >= offset.left && e.pageX <= offset.left + width)
-                option.top(target);
-            else if (e.pageY >= offset.top && e.pageY <= offset.top + height && e.pageX > offset.left + width)
-                option.right(target);
-            else if (e.pageX < offset.left && e.pageY > offset.top && e.pageY < offset.top + height)
-                option.left(target);
-            else if (e.pageX >= offset.left && e.pageX <= offset.left + width && e.pageY > offset.top + height)
-                option.bottom(target);
-            else if (e.pageX < offset.left && e.pageY < offset.top)
-                option.topLeft(target);
-            else if (e.pageX > offset.left + width && e.pageY < offset.top)
-                option.topRight(target);
-            else if (e.pageX < offset.left && e.pageY > offset.top + height)
-                option.bottomLeft(target);
-            else if (e.pageX > offset.left + width && e.pageY > offset.top + height)
-                option.bottomRight(target);
-            else if (e.pageX >= offset.left && e.pageX <= offset.left + width && e.pageY >= offset.top && e.pageY <= offset.top + height)
-                option.center(target);
+            // Mouse pos.
+            var mouseX   = e.pageX,
+                mouseY   = e.pageY
+                position = null;
+
+            // Find positions so make code more readable.
+            var onTop              = mouseY < topEdge,
+                onLeft             = mouseX < leftEdge,
+                onRight            = mouseX > rightEdge,
+                onBottom           = mouseY > bottomEdge,
+                horizontallyInside = mouseX >= leftEdge && mouseX <= rightEdge,
+                verticallyInside   = mouseY >= topEdge && mouseY <= bottomEdge;
+
+            // Switch true.
+            switch (true){
+
+                // Top
+                case onTop && horizontallyInside:
+                    position = "top";
+                    break;
+
+                // Left
+                case onLeft && verticallyInside:
+                    position = "left";
+                    break;
+
+                // Right
+                case verticallyInside && onRight:
+                    position = "right";
+                    break;
+
+                // Bottom
+                case horizontallyInside && onBottom:
+                    position = "bottom";
+                    break;
+
+                // TopLeft
+                case onLeft && onTop:
+                    position = "topLeft";
+                    break;
+
+                // TopRight
+                case onRight && onTop:
+                    position = "topRight";
+                    break;
+
+                // BottomLeft
+                case onLeft && onBottom:
+                    position = "bottomLeft";
+                    break;
+
+                // BottomRight
+                case onRight && onBottom:
+                    position = "bottomRight";
+                    break;
+
+                // Center
+                case horizontallyInside && verticallyInside:
+                    position = "center";
+                    break;
+            }
+
+            // Call position-assigned function with target context and pass target also as jQuery object.
+            if (position !== null && typeof option[position] === "function") option[position].call(target, $target)
+
+            // Call always with position info with same style.
+            option.always.call(target, $target, position);
 
         });
 
